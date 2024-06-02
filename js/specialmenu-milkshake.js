41640cc213f8dbdb5b2a81235367c6e8f00b4325
ParniaHabibi97 <parniahabibi1997@gmail.com>
2024-06-02 14:36:18 -0700

    special menu for drinks added

--- js/specialmenu-milkshake.js
+++ js/specialmenu-milkshake.js
@@ -1,14 +1,14 @@
 function updateCoffeePicture() {
     const ingredient = document.getElementById('ingredient-select').value;
     const coffeeImage = document.getElementById('coffee-image');
-    if (ingredient === 'latte') {
-        coffeeImage.src = 'images/Latte-coffee.jpg';
-    } else if (ingredient === 'Americano') {
-        coffeeImage.src = 'images/Americano.jpg';
-    } else if (ingredient === 'mocha') {
-        coffeeImage.src = 'images/Mocha-coffee.jpg';
-    } else if (ingredient === 'Affogato') {
-        coffeeImage.src = 'images/Affogato-coffee.jpg';
+    if (ingredient === 'Bannana') {
+        coffeeImage.src = 'images/banana-milkshake.jpg';
+    } else if (ingredient === 'Chocolate') {
+        coffeeImage.src = 'images/chocolate-milkshake.jpg';
+    } else if (ingredient === 'Strawberry') {
+        coffeeImage.src = 'images/strawberry-milkshake.jpg';
+    } else if (ingredient === 'Vanilla') {
+        coffeeImage.src = 'images/vanilla-milkshake.jpg';
     } else {
         coffeeImage.src = 'images/Coffee.jpg';
     }
